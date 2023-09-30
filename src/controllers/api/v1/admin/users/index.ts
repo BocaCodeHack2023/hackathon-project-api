ee62da60fb93dc7d898732df5f6b21a5a5f67821
MarioD <mario@mario.com>
2023-09-30 13:00:21 -0400

    fix

--- src/controllers/api/v1/admin/users/index.ts
+++ src/controllers/api/v1/admin/users/index.ts
@@ -5,6 +5,19 @@ import { inspect } from "../../../../../utils/helper";
 const moduleName = "src/controllers/api/v1/admin/users/index";
 const logger = getLogger(moduleName);
 
+
+// list all users
+export function index(req: any, res: any): any {
+  const methodName = "index";
+
+  logger.info({ moduleName, methodName }, "Start!");
+
+  const coin: string = req.body["coin"] ? req.body["coin"].toUpperCase() : "";
+
+  logger.debug({ moduleName, methodName }, "body:", inspect(req.body));
+}
+
+// create a new user
 export function create(req: any, res: any): any {
   const methodName = "create";
 
@@ -13,29 +26,23 @@ export function create(req: any, res: any): any {
   const coin: string = req.body["coin"] ? req.body["coin"].toUpperCase() : "";
 
   logger.debug({ moduleName, methodName }, "body:", inspect(req.body));
-
-  // _setModel(coin)
-  //   .create(coin)
-  //   .then((response: any) => {
-  //     res.send(response);
-  //   })
-  //   .catch((error: any) => {
-  //     console.log(error);
-  //   });
 }
 
+// update a user
 export function update(req: any, res: any): any {
   const methodName = "update";
 
   logger.info({ moduleName, methodName }, "Start!");
 }
 
+// display one user
 export function show(req: any, res: any): any {
   const methodName = "show";
 
   logger.info({ moduleName, methodName }, "Start!");
 }
 
+// destroy user from db
 export function destroy(req: any, res: any): any {
   const methodName = "destroy";
 
